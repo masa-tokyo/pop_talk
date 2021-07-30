448c9f146d0f3e30d4861c9c2376ae769c3ba0a2
yam-yanu <you1095@gmail.com>
2021-07-30 23:07:59 +0900

    [Fix] Debugモードでホットリロードするとエラーになる問題を修正

--- lib/infrastructure/service_provider.dart
+++ lib/infrastructure/service_provider.dart
@@ -7,6 +7,7 @@ import 'package:pop_talk/infrastructure/repository/firestore/talk_topic.dart';
 Future<bool> registerDIContainer() async {
   await dotenv.load();
   final getIt = GetIt.instance;
+  await getIt.reset();
   _registerRepository(getIt);
   return true;
 }
