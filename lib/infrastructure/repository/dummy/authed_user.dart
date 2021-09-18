0fa4e8e752babf3811c78d8c596beeb664ab47f3
Yuta <ezochi66@gmail.com>
2021-09-18 12:55:15 +0900

    共通部分の変数化

--- lib/infrastructure/repository/dummy/authed_user.dart
+++ lib/infrastructure/repository/dummy/authed_user.dart
@@ -2,68 +2,38 @@ import 'package:pop_talk/domain/model/authed_user.dart';
 import 'package:pop_talk/domain/repository/authed_user.dart';
 
 class DummyAuthedUserRepository implements AuthedUserRepository {
+  AuthedUser authedUser = AuthedUser(
+    id: '1',
+    name: 'テスト太郎',
+    isAnonymous: false,
+    followingUserIds: ['5', '2', '8'],
+    likeTalkIds: ['1', '3', '5', '6'],
+    likeNumber: 5,
+    followerNumber: 2,
+  );
+
   @override
   Future<AuthedUser> implicitLogin() async {
-    return AuthedUser(
-      id: '1',
-      name: 'テスト太郎',
-      isAnonymous: false,
-      followingUserIds: ['5', '2', '8'],
-      likeTalkIds: ['1', '3', '5', '6'],
-      likeNumber: 5,
-      followerNumber: 2,
-    );
+    return authedUser;
   }
 
   @override
   Future signUpWithGoogle() async {
-    return AuthedUser(
-      id: '1',
-      name: 'テスト太郎',
-      isAnonymous: false,
-      followingUserIds: ['5', '2', '8'],
-      likeTalkIds: ['1', '3', '5', '6'],
-      likeNumber: 5,
-      followerNumber: 2,
-    );
+    return authedUser;
   }
 
   @override
   Future signInWithGoogle() async {
-    return AuthedUser(
-      id: '1',
-      name: 'テスト太郎',
-      isAnonymous: false,
-      followingUserIds: ['5', '2', '8'],
-      likeTalkIds: ['1', '3', '5', '6'],
-      likeNumber: 5,
-      followerNumber: 2,
-    );
+    return authedUser;
   }
 
   @override
   Future signUpWithApple() async {
-    return AuthedUser(
-      id: '1',
-      name: 'テスト太郎',
-      isAnonymous: false,
-      followingUserIds: ['5', '2', '8'],
-      likeTalkIds: ['1', '3', '5', '6'],
-      likeNumber: 5,
-      followerNumber: 2,
-    );
+    return authedUser;
   }
 
   @override
   Future signInWithApple() async {
-    return AuthedUser(
-      id: '1',
-      name: 'テスト太郎',
-      isAnonymous: false,
-      followingUserIds: ['5', '2', '8'],
-      likeTalkIds: ['1', '3', '5', '6'],
-      likeNumber: 5,
-      followerNumber: 2,
-    );
+    return authedUser;
   }
 }
